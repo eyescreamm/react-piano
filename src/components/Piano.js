ee2b2d942ecc757de5352699d1e2256e0c239f83
ShutoYoshida <yoshidashi058@gmail.com>
2022-09-05 16:35:48 +0900

    add key handle and audio

--- src/components/Piano.js
+++ src/components/Piano.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Key from './Key.js';
 import _ from 'lodash';
 import './Piano.css'; 
 import { NOTES } from '../grobal/constants.js';
+import { VALID_KEYS } from '../grobal/constants.js';
+import { KEY_TO_NOTE } from '../grobal/constants.js';
 
 const Piano = () => {
-  const [pressedKeys, setPressedKeys] = React.useState([]);
+  const [pressedKeys, setPressedKeys] = useState([]);
+  
+  // Run only on first render ([] <- Run only on change)
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+  }, []);
+
+  const handleKeyDown = (event) => {
+    if (event.repeat) {
+      return;
+    }
+    const key = event.key;
+    const updatedPressedKeys = pressedKeys;
+    if (!updatedPressedKeys.includes(key) && VALID_KEYS.includes(key)) {
+      updatedPressedKeys.push(key);
+    }
+    setPressedKeys(updatedPressedKeys);
+    playNote(KEY_TO_NOTE[key]);
+    console.log(updatedPressedKeys);
+  }
+
+  const handleKeyUp = (event) => {
+    const index = pressedKeys.indexOf(event.key);
+    if (index > -1) {
+      setPressedKeys(pressedKeys.splice(index, 1));
+      console.log("a");
+    }
+    console.log(pressedKeys);
+  }
+
+  const playNote = (note) => {
+    if (!_.isEmpty(note)) {
+      console.log("play");
+      const noteAudio = new Audio(document.getElementById(note).src);
+      noteAudio.play();
+    }
+  }
 
   const Keys = _.map(NOTES, (note, index) => {
     return (
@@ -17,9 +56,24 @@ const Piano = () => {
     )
   });
 
+  const audioFiles = _.map(NOTES, (note, index) => {
+    return (
+      <audio 
+        id={note}
+        key={index}
+        src={'../../notes/' + note + '.mp3'}
+      />
+    )
+  });
+
   return(
-    <div className="piano">
-      {Keys}
+    <div>
+      <div className="piano">
+        {Keys}
+      </div>
+      <div>
+        {audioFiles}
+      </div>
     </div>
   );
 }
