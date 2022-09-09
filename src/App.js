8b145df6edb8282c372b568f7a5cf55756a9784c
eyescreamm <yoshidashi058@gmail.com>
2022-09-09 23:47:17 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Piano from './components/Piano';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [c, setc] = useState(0)
+  
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+  }, []);
+
+  const handleKeyDown = (event) => {
+    const count = c + 1
+    setc(count)
+    console.log(count)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
